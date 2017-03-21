fe75a3faf713ca5ecc5f87f713c82a930fe42bbe
mohammed merajuddin <meraj6054@gmail.com>
2017-03-21 00:49:36 -0500

    jquery and bootstrap

---
+++ BootStrapJqueryProtek/main.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+   
+    //init function
+    function init() {
+        getdata();
+
+        // fade in effect;
+        $(".bottom-section").fadeOut().fadeIn(2000);
+    }
+
+    // call init
+    init();
+
+
+
+});
+   
+// ajax function
+function getdata() {
+    $.ajax({
+        url: "data.json",
+        method: "GET",
+        success: function (resp) {
+            $.each(resp.HomeNav_Links, function (index, value) {
+                $(".navbar-nav").append(
+                    '<li><a>' + value + '</a></li>'
+                    );
+            })          
+        }
+    })
+
+}
